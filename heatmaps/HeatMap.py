46f460479377a0de8bd816cc1f6505226210015d
ArjunKhattry <arjun7.mitmpl2022@learner.manipal.edu>
2025-07-23 10:16:56 +0530

    Update HeatMap.py - 23JUL

--- heatmaps/HeatMap.py
+++ heatmaps/HeatMap.py
@@ -4,35 +4,28 @@ from ultralytics import YOLO
 import os
 from datetime import datetime, timedelta
 import json
-from PIL import Image
 import atexit
+from PIL import Image, ImageDraw
 from collections import defaultdict
 
-CONFIG_PATH = "alert_config.json"
-if not os.path.exists(CONFIG_PATH):
-    default_config = {
-        "alert_threshold": 1,
-        "alert_zones": {
-            "0": [[200, 140], [280, 260]]
-        }
-    }
-    with open(CONFIG_PATH, "w") as f:
-        json.dump(default_config, f, indent=4)
-
+CONFIG_PATH = "engine/alert_config.json"
+CONFIG_STATUS = 0
 HEAT_INCREMENT = 40
-FIXED_SIZE = (640, 480)
+FIXED_SIZE = (1032, 616)
 CONFIDENCE_THRESHOLD = 0.4
 DUPLICATE_DISTANCE_THRESHOLD = 80
 MIN_MOVEMENT_THRESHOLD = 60
+BLOB_THRESHOLD_DURATION = 10
 BLOB_LOG_DISTANCE_THRESHOLD = 10
-
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "yolov8n.pt")
-model = YOLO(MODEL_PATH)
 
-bg_image_path = "/home/cbginnovation/Desktop/cam0.png"
+model = YOLO(MODEL_PATH)
+bg_image_path = "background.jpg"
+
 if not os.path.exists(bg_image_path):
     print(f"Error: Background image not found at {bg_image_path}")
     exit(1)
+
 background_image = cv2.imread(bg_image_path)
 background_image = cv2.resize(background_image, FIXED_SIZE)
 h, w = background_image.shape[:2]
@@ -46,10 +39,13 @@ last_seen_time = {}
 temp_blob_log = []
 previous_logged_count = -1
 alert_triggered = False
+alert_zone_person = 0
+last_logged_blob_positions = {}
+last_logged_blob_positions["last_logged_time"] = {}
 
-os.makedirs("heatmaps", exist_ok=True)
-person_json_path = "heatmaps/person_count_log.json"
-blob_json_path = "heatmaps/heatmap_log.json"
+os.makedirs("database", exist_ok=True)
+person_json_path = "database/person_count_log.json"
+blob_json_path = "database/heatmap_log.json"
 for path in [person_json_path, blob_json_path]:
     if not os.path.exists(path):
         with open(path, "w") as f:
@@ -62,6 +58,12 @@ def append_json(filepath, data_entry):
     with open(filepath, "w") as f:
         json.dump(data, f, indent=4)
 
+def update_alertconfig(data):
+    global CONFIG_STATUS
+    with open(CONFIG_PATH, "w") as f:
+        json.dump(data, f, indent=4)
+        CONFIG_STATUS = 1
+
 def deduplicate_points(points, threshold):
     unique = []
     for pt in points:
@@ -72,7 +74,9 @@ def deduplicate_points(points, threshold):
 def load_alert_config():
     with open(CONFIG_PATH, "r") as f:
         config = json.load(f)
-    return config["alert_threshold"], {int(k): v for k, v in config["alert_zones"].items()}
+    return config["alert_threshold"], config["alert_zones"]
+
+ALERT_THRESHOLD, zone = load_alert_config()
 
 def save_blob_log():
     if temp_blob_log:
@@ -109,8 +113,6 @@ def get_today_max_person_count_by_30min():
 
     return intervals
 
-
-
 def get_max_person_count_by_30min():
     with open(person_json_path, "r") as f:
         data = json.load(f)
@@ -129,17 +131,18 @@ def get_max_person_count_by_30min():
 atexit.register(save_blob_log)
 
 def process_frame(frame):
-    global last_positions, position_duration, last_seen_time
-    ALERT_THRESHOLD, alert_zones = load_alert_config()
-
+    global last_positions, last_logged_blob_positions, ALERT_THRESHOLD, zone, CONFIG_STATUS
     frame = cv2.resize(frame, FIXED_SIZE)
+    output = frame.copy()
     results = model(frame, verbose=False)[0]
     current_detections = []
     zone_count = 0
     current_time = datetime.now()
-
-    zone = alert_zones.get(0)
     new_last_positions = {}
+
+    if CONFIG_STATUS == 1:
+        ALERT_THRESHOLD, zone = load_alert_config()
+        CONFIG_STATUS = 0
 
     for idx, box in enumerate(results.boxes):
         cls_id = int(box.cls[0])
@@ -159,15 +162,9 @@ def process_frame(frame):
             dist = np.hypot(cx - prev_pos[0], cy - prev_pos[1]) if prev_pos else float('inf')
 
             if prev_pos is None or dist >= MIN_MOVEMENT_THRESHOLD:
-                position_duration[key] = 0
+                position_duration[key] = 1
                 last_seen_time[key] = current_time
-            else:
-                elapsed = (current_time - last_seen_time.get(key, current_time)).total_seconds()
-                position_duration[key] = int(elapsed)
 
-            new_last_positions[key] = (cx, cy)
-
-            if prev_pos is None or np.hypot(cx - prev_pos[0], cy - prev_pos[1]) >= BLOB_LOG_DISTANCE_THRESHOLD:
                 intensity = HEAT_INCREMENT
                 persistent_heatmap[cy, cx] = min(persistent_heatmap[cy, cx] + intensity, 255)
                 temp_blob_log.append({
@@ -176,12 +173,17 @@ def process_frame(frame):
                     "x": cx,
                     "y": cy
                 })
+            else:
+                elapsed = (current_time - last_seen_time.get(key, current_time)).total_seconds()
+                position_duration[key] = int(elapsed)
+
+            new_last_positions[key] = (cx, cy)
 
             if zone:
                 (xA, yA), (xB, yB) = zone
                 if xA <= cx <= xB and yA <= cy <= yB:
                     zone_count += 1
-
+            
             current_detections.append((cx, cy))
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
@@ -194,11 +196,13 @@ def process_frame(frame):
             else:
                 timer_color = (0, 0, 255)
 
-            cv2.putText(frame, f"{duration}s", (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, timer_color, 2)
+            ##### corner
+            cv2.rectangle(frame, (x1, y2-36), (x1+120, y2-2), (80,80,80), -1)
 
+            ##### putText
+            cv2.putText(frame, f" Idle {duration}s", (x1, y2-10), cv2.FONT_HERSHEY_DUPLEX, 0.7, timer_color, 2)
+    
     last_positions = new_last_positions
-
     if zone:
         (xA, yA), (xB, yB) = zone
         cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 0, 255), 2)
@@ -206,8 +210,10 @@ def process_frame(frame):
     return frame, current_detections, zone_count
 
 def generate_processed_frames():
-    global cam, previous_logged_count, alert_triggered
+    global cam, previous_logged_count, alert_triggered, ALERT_THRESHOLD, alert_zone_person
     cam = cv2.VideoCapture(0)
+    # cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1000)
+    # cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
     if not cam.isOpened():
         print("\u274c Webcam not accessible")
         return
@@ -229,7 +235,8 @@ def generate_processed_frames():
         cv2.putText(overlay, f"Live Person Count: {total_detected}", (20, 40),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 3)
 
-        ALERT_THRESHOLD, _ = load_alert_config()
+        alert_zone_person = zone_count
+
         if zone_count > ALERT_THRESHOLD:
             alert_triggered = True
             cv2.putText(overlay, "ALERT: Overcrowding!", (20, 80),
